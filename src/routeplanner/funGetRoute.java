b115c2f0cf18fc9d94a5fa167a551bfd3c7d0126
Rasmus <rallez@gmail.com>
2014-11-04 21:32:24 +0100

    add pseudocode

--- src/routeplanner/funGetRoute.java
+++ src/routeplanner/funGetRoute.java
@@ -17,12 +17,23 @@ public class funGetRoute implements Userfunction {
 		engine = context.getEngine();
 		System.out.println("Current goals are:");
 		
+		// Go through facts and find goal facts
 		Iterator facts = engine.listFacts();
 		while(facts.hasNext())
 		{
 			Fact fact = (Fact) facts.next();
 			System.out.println(fact.toString());
 		}
+		
+		// Pseudocode for route plan:
+		
+		// Calculate shortest route between current position and all n goals:
+			// foreach goal in goals:
+				// a-star(current, goal)
+				// foreach other_goal in goals != goal:
+					// a-star(goal, other_goal)
+		// Bruteforce TSP problem:
+			// todo
 		
 		return jess.Funcall.NIL;
 	}
